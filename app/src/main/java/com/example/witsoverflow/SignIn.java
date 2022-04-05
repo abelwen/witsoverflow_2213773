c43cf0d12e79dd9b4989a598294dce4119a68502
PulengAlvi <2213773@students.wits.ac.za>
2022-04-05 13:43:50 +0200

    puleng

---
+++ app/src/main/java/com/example/witsoverflow/SignIn.java
@@ -0,0 +1,13 @@
+package com.example.witsoverflow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
