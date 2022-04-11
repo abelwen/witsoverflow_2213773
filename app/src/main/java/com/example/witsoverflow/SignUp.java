20a740f77a7dfbe073a8f9ea1307684eb6e9a88e
Sandile-N <69665260+Sandile-N@users.noreply.github.com>
2022-04-11 12:12:09 +0000

    Update SignUp.java

--- app/src/main/java/com/example/witsoverflow/SignUp.java
+++ app/src/main/java/com/example/witsoverflow/SignUp.java
@@ -45,7 +45,7 @@ public class SignUp extends AppCompatActivity {
     }
     public void checkUserData(){
         boolean isValid = true;
-        if (password.getText().toString().equals(re_password.getText().toString())) { //checks if the password matches the re-enter
+        if (!password.getText().toString().equals(re_password.getText().toString())) { //checks if the password matches the re-enter
                 password.setError("Password and Re-enter Password must match!");
                 isValid = false;
         }
