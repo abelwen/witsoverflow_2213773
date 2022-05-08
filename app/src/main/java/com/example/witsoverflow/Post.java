e4e8b7dcc67899123436601379320623446ce776
Rofhiwa67 <101204347+Rofhiwa67@users.noreply.github.com>
2022-05-08 20:33:46 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/witsoverflow/Post.java
@@ -0,0 +1,41 @@
+package com.example.witsoverflow;
+
+public class Post {
+
+    String username, caption, post, tags, timestamp;
+    int upvote, downvote;
+    public String getusername() {
+
+        return username;
+    }
+
+    public String getCaption() {
+
+        return caption;
+    }
+
+    public String getTags() {
+
+        return tags;
+    }
+
+    public String getTimestamp() {
+
+        return timestamp;
+    }
+
+    public int getUpvote() {
+
+        return upvote;
+    }
+
+    public int getDownvote() {
+
+        return downvote;
+    }
+
+    public String getPost() {
+
+        return post;
+    }
+}
