f51a02cfe1e85d07b52d372745bbb305066d2125
Rofhiwa67 <101204347+Rofhiwa67@users.noreply.github.com>
2022-05-08 20:24:05 +0200

    Update MyAdapter.java

--- app/src/main/java/com/example/witsoverflow/MyAdapter.java
+++ app/src/main/java/com/example/witsoverflow/MyAdapter.java
@@ -29,7 +29,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) { //sets 
 
         Post pst = list.get(position);
         String x,y;
@@ -46,7 +46,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-
+    
         TextView post, username, up, down;
 
         public MyViewHolder(@NonNull View itemView) {
