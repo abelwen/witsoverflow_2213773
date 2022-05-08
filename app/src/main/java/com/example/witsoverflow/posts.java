467c701aa3f1a0fda09b0530f5c55183474e2532
Rofhiwa67 <101204347+Rofhiwa67@users.noreply.github.com>
2022-05-08 21:43:10 +0200

    Update posts.java

--- app/src/main/java/com/example/witsoverflow/posts.java
+++ app/src/main/java/com/example/witsoverflow/posts.java
@@ -25,3 +25,35 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class posts extends AppCompatActivity {
+
+    DatabaseReference database;
+    //FirebaseDatabase firebaseDatabase;
+
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+    ArrayList<Post> list;
+    ImageButton add, mypro;
+    SearchView tags;
+    //String Student_num = getIntent().getStringExtra("Stud_Num"); //so we know who's online
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+        recyclerView = findViewById(R.id.postlist);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab_btn);
+        String Student_num = getIntent().getStringExtra("Stud_Num");
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(posts.this,CreatePost.class);
+                switchActivityIntent.putExtra("Stud_Num",Student_num);
+                startActivity(switchActivityIntent);
+            }
+        });
