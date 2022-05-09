a23a4e8f5798c2fde9ff3302a3f5b0577cb9be5a
TRJMajika <101942468+TRJMajika@users.noreply.github.com>
2022-05-09 13:14:59 +0200

    Update VotingFunctions.java

--- app/src/main/java/com/example/witsoverflow/VotingFunctions.java
+++ app/src/main/java/com/example/witsoverflow/VotingFunctions.java
@@ -1,35 +1,35 @@
 package com.example.witsoverflow;      //////sends and gets votes, to be used in any class that instantiates this class
-
+                                       //one of the functions in here increments the num of votes, up or down
 public class VotingFunctions {     
     public int upVotes;
     public int dnVotes;
 
-    public VotingFunctions(int upVotes, int dnVotes) {
+    public VotingFunctions(int upVotes, int dnVotes) {  //constructor
         this.upVotes = upVotes;
         this.dnVotes = dnVotes;
     }
 
-    public int getUpVotes() {
+    public int getUpVotes() {   //returns the up vote
         return upVotes;
     }
 
-    public void setUpVotes(int upVotes) {
+    public void setUpVotes(int upVotes) {   //sets the up vote
         this.upVotes = upVotes;
     }
 
-    public int getDnVotes() {
+    public int getDnVotes() {   //returns the down vote
         return dnVotes;
     }
 
-    public void setDnVotes(int dnVotes) {
+    public void setDnVotes(int dnVotes) {   
         this.dnVotes = dnVotes;
     }
 
-    public int i_upvotes(){
+    public int i_upvotes(){    
         return  upVotes+1;
     }
 
-    public int i_downvotes(){
+    public int i_downvotes(){   
         return  dnVotes+1;
     }
 }
