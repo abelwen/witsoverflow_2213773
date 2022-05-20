34b8791ada9f16650ddf25833f8f8f18a1cef8e6
abelwen <96735865+abelwen@users.noreply.github.com>
2022-05-20 13:12:21 +0000

    Update SignUpTest.java

--- app/src/androidTest/java/com/example/witsoverflow/SignUpTest.java
+++ app/src/androidTest/java/com/example/witsoverflow/SignUpTest.java
@@ -1,37 +1,44 @@
 package com.example.witsoverflow;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertNotNull;
+
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-
-import com.example.witsoverflow.SignUp;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+//
 
-public class SignUpTest
-{
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+//
+public class SignUpTest{
+
     @Rule
     public ActivityScenarioRule<SignUp> tSignUpScenarioRule = new ActivityScenarioRule<SignUp>(SignUp.class);
-
     private ActivityScenario<SignUp> tSignUp = null;
 
     @Before
     public void setUp() throws Exception {
-
         tSignUp = tSignUpScenarioRule.getScenario();
-
     }
 
     @Test
 
-    public void testLaunch(){
+    public void testLaunch() {
 
-        assertNotNull( onView(withId(R.id.Name)));
+        assertNotNull(onView(withId(R.id.Name)));
         assertNotNull(onView(withId(R.id.Surname)));
         assertNotNull(onView(withId(R.id.email)));
         assertNotNull(onView(withId(R.id.cell_num)));
@@ -45,6 +52,7 @@ public class SignUpTest
     @After
     public void tearDown() throws Exception {
 
-        tSignUp = null ;
+        tSignUp = null;
     }
 }
+
