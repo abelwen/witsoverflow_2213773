1246cc1acd29dfaca3ca63b943702d490ba21f1f
Rampamba Lingedzani <102157126+RampLingedzani@users.noreply.github.com>
2022-05-23 12:58:17 +0200

    Update MyAdapter.java

--- app/src/main/java/com/example/witsoverflow/MyAdapter.java
+++ app/src/main/java/com/example/witsoverflow/MyAdapter.java
@@ -1,61 +1,14 @@
 package com.example.witsoverflow;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-
-    Context context;
-    ArrayList<Post> list;
-
-    public MyAdapter(Context context, ArrayList<Post> list) {
-         this.list = list;
-        this.context = context;
-       
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.item,parent,false);
-        return new MyViewHolder(v);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) { //sets 
-        String x,y;
-        Post pst = list.get(position);
-        holder.username.setText(pst.getusername());
-        holder.post.setText(pst.getPost());
-        holder.up.setText((String.valueOf(pst.getUpvote())));
-        holder.down.setText((String.valueOf(pst.getDownvote())));
-        }
-
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-
-    public static class MyViewHolder extends RecyclerView.ViewHolder{
-    
-        TextView post, username, up, down;
-
-        public MyViewHolder(@NonNull View itemView){
-            super(itemView);
-
-            up = itemView.findViewById(R.id.upVoteNo);
-            down = itemView.findViewById(R.id.downVoteNo);
-            username = itemView.findViewById(R.id.userName);
-            post = itemView.findViewById(R.id.post);
-        }
-    }
-
-}
