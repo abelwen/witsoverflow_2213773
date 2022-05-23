38fb82bda0aedee604b88a7452dcb6a24b510267
Rampamba Lingedzani <102157126+RampLingedzani@users.noreply.github.com>
2022-05-23 12:59:07 +0200

    Update MyAdapter.java

--- app/src/main/java/com/example/witsoverflow/MyAdapter.java
+++ app/src/main/java/com/example/witsoverflow/MyAdapter.java
@@ -12,3 +12,35 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Post> list;
+    TextView post, unam;
+
+    public MyAdapter(Context context, ArrayList<Post> list) {
+        this.context = context;
+        this.list = list;
+    }
+  
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item,parent,false);
+        return new MyViewHolder(v);
+    }
+
+  @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Post pst = list.get(position);
+        String x,y;
+
+        holder.username.setText(pst.getusername());
+        holder.post.setText(pst.getPost());
+        holder.up.setText((String.valueOf(pst.getUpvote())));
+        holder.down.setText((String.valueOf(pst.getDownvote())));
+
+        }
+  
